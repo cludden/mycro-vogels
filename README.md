bdbdef5b9bf19b8f72e0f2880f0247887806cdda
cludden <chris.ludden@gmail.com>
2016-01-24 15:28:32 -0700

    refactor

--- README.md
+++ README.md
@@ -1,7 +1,68 @@
-# restify-microservice-vogels
-a [vogels](https://github.com/ryanfitz/vogels) (DynamoDB ODM) adapter for [restify-microservice](https://github.com/cludden/restify-microservice)
+# mycro-vogels
+a [dynamodb](https://aws.amazon.com/dynamodb) adapter (using [vogels](https://github.com/ryanfitz/vogels)) for [mycro](https://github.com/cludden/mycro)
 
 ## Install
 ```javascript
-npm install --save restify-microservice-vogels
+npm install --save vogels mycro-vogels
+```
+
+## Getting Started
+1. Make sure your aws credentials are available (environment variables, ec2 instance profile, or set manually)
+2. Define a connection
+
+```javascript
+// in config/connections.js
+
+var adapter = require('mycro-vogels');
+
+module.exports = {
+    vogels: {
+        adapter: adapter,
+        config: {
+            region: 'us-west-2'
+        }
+    }
+}
+```
+3. Define a model
+
+```javascript
+// in app/models/post.js
+
+module.exports = function(vogels, joi, name) { // name = 'post'
+    let model = vogels.define(name, {
+        hashKey: 'email',
+        rangeKey: 'title',
+        tableName: 'blogPosts',
+        schema: {
+            email: joi.string().email(),
+            title: joi.string(),
+            content: joi.binary(),
+            tags: vogels.types.stringSet()
+        }
+    });
+
+    return model;
+};
+```
+4. Use it
+
+```javascript
+// in app/controllers/posts.js
+
+module.exports = {
+    create(req, res) {
+        let Posts = req.mycro.models.post;
+        Posts.create({
+            email: req.body.email,
+            title: req.body.title,
+            content: req.body.content
+        }, function (err, post) {
+            if (err) {
+                return res.json(500, {error: err});
+            }
+            res.json(200, {data: post.toJSON()});
+        });
+    }
+};
 ```
--- index.js
+++ index.js
@@ -1,42 +1,55 @@
 'use strict';
 
-var vogels = require('vogels');
+var vogels = require('vogels'),
+    joi = require('joi'),
+    _ = require('lodash');
 
 module.exports = {
-
-    registerConnection: function(connectionInfo, cb) {
-
+    /**
+     * Configure vogels
+     *
+     * @param  {Object} config
+     * @param  {String} config.region
+     * @param  {Function} cb
+     */
+    registerConnection(config, cb) {
         // for dev, otherwise IAM roles should be used
         if(process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
-            connectionInfo.accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-            connectionInfo.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+            config.accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+            config.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
         }
 
-        // configure vogels
-        if(!connectionInfo.region) {
-            cb(new Error('missing required connection property: region'));
-        } else {
-            vogels.AWS.config.update(connectionInfo);
-            cb(null, vogels);
+        // ensure that `region` is defined
+        if(!config.region) {
+            return cb(new Error('missing required connection property: region'));
         }
+
+        // configure vogels and return
+        vogels.AWS.config.update(config);
+        cb(null, vogels);
     },
 
-    registerModel: function(connection, modelDefinition, cb) {
 
-        var name = modelDefinition.name,
-            model;
-        delete modelDefinition.name;
-
-        // create model
-        if(!name || !modelDefinition.hashKey || !modelDefinition.schema) {
-            cb(new Error('missing at least one required model property: name, hashKey, or schema'));
-        } else if(!modelDefinition.schema[modelDefinition.hashKey]) {
-            cb(new Error('hashKey missing from schema'));
-        } else {
-            model = connection.define(name, modelDefinition);
-            cb(null, model);
+    /**
+     * Register vogel model
+     *
+     * @param  {Object} vogels
+     * @param  {Function} factory
+     * @param  {Function} cb
+     */
+    registerModel: function(vogels, factory, name, cb) {
+        if (!_.isFunction(factory)) {
+            return cb(new Error(`Invalid model definition provided for
+                model '${name}'. Model modules should export a factory
+                function that accepts a configured vogels instance, a
+                joi instance, and the name of the model.`
+            ));
         }
-
+        try {
+            let model = factory(vogels, joi, name);
+            return cb(null, model);
+        } catch (e) {
+            cb(e);
+        }
     }
-
 };
